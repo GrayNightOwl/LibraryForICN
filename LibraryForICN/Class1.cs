e31474dbdb33336bd6d69bc2695a7b08fba83111
Сергей <serega-k97@yandex.ru>
2018-08-17 22:18:56 +0500

    Переписано с использованием классов, переписан метод "регион", метод "район" пока не работает,

--- LibraryForICN/Class1.cs
+++ LibraryForICN/Class1.cs
@@ -7,148 +7,182 @@ using System.Text.RegularExpressions;
 
 namespace LibraryForICN
 {
+
+    public class AdressForCompile
+    {
+        public bool CorrectAdress;
+        public string index;
+        public string region;
+        public string area;
+        public string city;
+        public string street;
+        public string house;
+        public string flat;
+    }
+
     public class Adress
     {
-        public string CompileAdress(string index, string region, string city, string street, string house, string flat)
+        public string CompileAdress(AdressForCompile adress)
         {
-            string adress = "";
-            adress += index + ", " + region + ", г. " + city + ", ул. " + street + ", д. " + house + ", кв. " + flat;
-            return adress;
-        }
+            string result = "";
 
-        public string CompileAdress(string index, string region, string area, string city, string street, string house, string flat)
-        {
-            string adress = "";
-            adress += index + ", " + region + ", " + area + ", г. " + city + ", ул. " + street + ", д. " + house + ", кв. " + flat;
-            return adress;
-        }
-
-
-
-
-
-
-        public string Street(string s)
-        {
-            s = "," + s + ",";
-            string street = "Не удалось распознать улицу";
-            Regex regex1 = new Regex(@",\s*(?:у|У)(?:[а-я]|-)*(?:\.|\s)+((?:[0-9]|[А-я]|-|\s)+)\s*,+.*"); //добавлены цифры
-            Regex regex2 = new Regex(@",\s*(?:у|У)(?:[а-я]|-)*\s*([А-Я](?:[А-я]|-|\s)+)\s*,+.*");
-            MatchCollection matches = regex1.Matches(s);
-
-            if (matches.Count > 0)
+            result = result + adress.index + ", ";
+            result = result + adress.region + ", ";
+            if (adress.area == "")
             {
-                foreach (Match match in matches)
+                result = result + adress.area + ", ";
+            }
+            result = result + "г. " + adress.city + ", ";
+            if (adress.CorrectAdress == true) //если адрес корректный - то улица и дом распознаны
+            {
+                result = result + "ул. " + adress.street + ", ";
+                result = result + "д. " + adress.house;
+                if (adress.flat != "Не удалось распознать квартиру")
                 {
-                    street = match.Groups[1].Value;
+                    result = result + ", кв. " + adress.flat;
                 }
+            }
+            else { result = "Некорректный адрес"; };
+
+            return result;
+        }
+
+        public AdressForCompile ParseAdress(string s)
+        {
+            AdressForCompile result = new AdressForCompile();
+            result.CorrectAdress = true;
+            result.index = Index(s);
+            result.region = Region(s); //может вернуть регион по умолчанию
+                                       // result.area = Area(s); //может вернуть район по умолчанию
+            result.city = City(s); //может вернуть город по умолчанию
+            result.street = Street(s); //если не указано - вернуть ошибку
+            result.house = House(s); //если не указано - вернуть ошибку
+            if ((result.street == "Не удалось распознать улицу") || (result.house == "Не удалось распознать дом"))
+            {
+                result.CorrectAdress = false;
+                return result;
             }
             else
-            {
-                MatchCollection matches2 = regex2.Matches(s);
-                if (matches2.Count > 0)
-                {
-                    foreach (Match match in matches2)
-                    {
-                        street = match.Groups[1].Value;
-                    }
-                }
-                //else
-                //    street = "Совпадений не найдено";
-            }
-            //if (street.IndexOf('-') !=(-1))
-            //{
-            //    street = street.Replace(" ", "");
-            //}
-            return street.Trim();
+                result.flat = Flat(s); //
+            return result;
         }
 
-        public string City(string s)
+
+
+
+
+
+
+        private string Index(string s)
         {
             s = "," + s + ",";
-            string city = "Не удалось распознать город";
+            string index = "";
+            Regex regex1 = new Regex(@",(\s*\d+\s*),+.*");
+            index = MatchWithOneRegex(regex1, s);
+            if (index == "") index = "Не удалось распознать индекс";
+            return index.Trim();
+        }
+
+        //private string Region(string s)
+        //{
+        //    s = "," + s + ",";
+        //    string region = "";
+        //    // string s = " д. 70/2, улСоловьева, кв. 45, край Пермский ,  614285   , город  Пермь , ";
+        //    Regex regex1 = new Regex(@",(\s*(?:[А-я]|-)*\s*(?:кр|КР|Кр|кР|о)+[А-я]*\s*(?:[А-я]|-)*)+\s*,+.*");
+        //    Regex regex2 = new Regex(@",(\s*(?:кр|Кр|КР|о)(?:[а-я]|-)*\s*[А-Я](?:[А-я]|-|\s)+)\s*,+.*");
+        //    region = MatchWithTwoRegex(regex1, regex2, s);
+        //    if (region == "") region = "Пермский край";
+        //    return region.Trim();
+        //}
+
+        private string Region(string s)
+        {
+            s = "," + s + ",";
+            string region = "";
+            // string s = " д. 70/2, улСоловьева, кв. 45, край Пермский ,  614285   , город  Пермь , ";
+            Regex regex1 = new Regex(@",(\s*(?:[А-я]|-)*\s+(?:кр|о)+[А-я]*\s*),+.*");
+            Regex regex2 = new Regex(@",(\s*(?:кр|о)+[А-я]*\s+(?:[А-я]|-)*)+\s*,+.*");
+            Regex regex3 = new Regex(@",(\s*(?:кр|о)(?:[а-я]|-)*\s*[А-Я](?:[А-я]|-|\s)+)\s*,+.*");
+            region = MatchWithTwoRegex(regex1, regex2, s);
+            if (region == "")
+            {
+                region = MatchWithOneRegex(regex3, s);
+            }
+            if (region == "") region = "Пермский край";
+            return region.Trim();
+        }
+
+        //private string Area(string s)
+        //{
+        //    s = ", " + s + " ,";
+        //    string area = "";
+        //    // string s = "614000, кв4 , дом 15,   улица. 1-я Красноармейская   , г. Москва,   край Пермский,район Пермский ,"
+        //    // string s = "614000, кв4 , дом 15,   улица. 1-я Красноармейская   , г. Москва,   край Пермский, Пермский район , "
+        //    //Regex regex1 = new Regex(@"(,\s*(?:[А-я]|-)*(?:,р|\s+р)[А-я]*\s*(?:[А-я]|-)*\s*),+.*")
+        //    Regex regex1 = new Regex(@",\s*");
+        //    Regex regex2 = new Regex(@",\s*р(?:[а-я]|-)*\s+([А-Я](?:[А-я]|-|\s)+)\s*,+.*");
+        //    area = MatchWithTwoRegex(regex1, regex2, s);
+        //    area.Replace(",", "");
+        //    return area.Trim();
+        //}
+
+        private string City(string s)
+        {
+            s = "," + s + ",";
+            string city = "";
 
             Regex regex1 = new Regex(@",\s*(?:г|Г)(?:[а-я]|-)*(?:\.|\s)+((?:[А-я]|-|\s)+)\s*,+.*");
-            Regex regex2 = new Regex(@",\s*(?:г|Г)(?:[а-я]|-)*\s*([А-Я](?:[А-я]|-|\s)+)\s*,+.*"); 
-            MatchCollection matches = regex1.Matches(s);
-
-
-            if (matches.Count > 0)
-            {
-                foreach (Match match in matches)
-                {
-                    city = match.Groups[1].Value;
-                }
-            }
-            else
-            {
-                MatchCollection matches2 = regex2.Matches(s);
-                if (matches2.Count > 0)
-                {
-                    foreach (Match match in matches2)
-                    {
-                        city = match.Groups[1].Value;
-                    }
-                }
-                else
-                    city = "Пермь";
-            }
+            Regex regex2 = new Regex(@",\s*(?:г|Г)(?:[а-я]|-)*\s*([А-Я](?:[А-я]|-|\s)+)\s*,+.*");
+            city = MatchWithTwoRegex(regex1, regex2, s); //если ошибка- вернётся пустая строка
+            if (city == "") city = "Пермь"; //если распознавание ничего не дало- вернём значение по умолчанию для города
             return city.Trim();
         }
 
-        public string House(string s)
+
+        private string Street(string s)
         {
             s = "," + s + ",";
-            string house = "Не удалось распознать дом";
+            string street = "";
+            Regex regex1 = new Regex(@",\s*(?:у|У)(?:[а-я]|-)*(?:\.|\s)+((?:[0-9]|[А-я]|-|\s)+)\s*,+.*"); //добавлены цифры
+            Regex regex2 = new Regex(@",\s*(?:у|У)(?:[а-я]|-)*\s*([А-Я](?:[А-я]|-|\s)+)\s*,+.*");
+            street = MatchWithTwoRegex(regex1, regex2, s);
+            if (street == "") street = "Не удалось распознать улицу";
+            return street.Trim();
+        }
+
+        private string House(string s)
+        {
+            s = "," + s + ",";
+            string house;
 
             Regex regex1 = new Regex(@",\s*(?:д|Д)(?:[А-я]|-)*(?:\.|\s)*(\d+(?:/|\s|[А-я])?\d*)\s*,+.*");
             MatchCollection matches = regex1.Matches(s);
-
-
-            if (matches.Count > 0)
-            {
-                foreach (Match match in matches)
-                {
-                    house = match.Groups[1].Value;
-                }
-            }
+            house = MatchWithOneRegex(regex1, s);
+            if (house == "") house = "Не удалось распознать дом";
             return house.Trim();
         }
 
-        public string Flat(string s)
+        private string Flat(string s)
         {
             s = "," + s + ",";
-            string flat = "Не удалось распознать квартиру";
-
+            string flat = "";
             Regex regex1 = new Regex(@",\s*(?:к|К)(?:[А-я]|-)*(?:\.|\s)*(\d+)\s*,+.*");
-            MatchCollection matches = regex1.Matches(s);
-
-
-            if (matches.Count > 0)
-            {
-                foreach (Match match in matches)
-                {
-                    flat = match.Groups[1].Value;
-                }
-            }
+            flat = MatchWithOneRegex(regex1, s);
+            if (flat == "") flat = "Не удалось распознать квартиру";
             return flat.Trim();
         }
 
 
-        public string Region(string s)
-        {
-            s = "," + s + ",";
-            string region = "Не удалось распознать регион";
-            Regex regex1 = new Regex(@",(\s*(?:[А-я]|-)*\s*(?:кр|КР|Кр|кР|о)+[А-я]*\s*(?:[А-я]|-)*)+\s*,+.*");
-            Regex regex2 = new Regex(@",\s*(?:кр|Кр|КР|о)(?:[а-я]|-)*\s*([А-Я](?:[А-я]|-|\s)+)\s*,+.*");
-            
-          MatchCollection matches = regex1.Matches(s);
 
+        private string MatchWithTwoRegex(Regex regex1, Regex regex2, string s)
+        {
+            string result = "";
+            MatchCollection matches = regex1.Matches(s);
             if (matches.Count > 0)
             {
                 foreach (Match match in matches)
                 {
-                    region = match.Groups[1].Value;
+                    result = match.Groups[1].Value;
                 }
             }
             else
@@ -158,16 +192,29 @@ namespace LibraryForICN
                 {
                     foreach (Match match in matches2)
                     {
-                        Console.WriteLine("Вторая ветвь");
-                        region = match.Groups[1].Value;
+                        result = match.Groups[1].Value;
                     }
                 }
-                else
-                    region = "Пермский край";
             }
-            return region.Trim();
-
+            return result;
         }
+
+        private string MatchWithOneRegex(Regex regex1, string s)
+        {
+            string result = "";
+            MatchCollection matches = regex1.Matches(s);
+
+
+            if (matches.Count > 0)
+            {
+                foreach (Match match in matches)
+                {
+                    result = match.Groups[1].Value;
+                }
+            }
+            return result;
+        }
+
 
     }
 }
