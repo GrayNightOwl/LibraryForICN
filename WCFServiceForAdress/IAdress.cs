43d64d132d5a4c99ac46110b13ea8ea531ea9184
Сергей <serega-k97@yandex.ru>
2018-08-18 23:56:56 +0500

    Начинаем использование функций библиотеки

--- WCFServiceForAdress/IAdress.cs
+++ WCFServiceForAdress/IAdress.cs
@@ -11,7 +11,7 @@ namespace WCFServiceForAdress
     [ServiceContract(Namespace = "AddressNS")]
     public interface IntAddress
     {
-        
+
         [OperationContract]
         string CompileAddressFromSet(string index, string region, string area, string city, string street, string house, string flat);
 
@@ -19,4 +19,18 @@ namespace WCFServiceForAdress
         string[] ParseAddress(string s);
 
     }
+
+    //[DataContract]
+    //public class AddressStructure
+    //{
+    //    public bool CorrectAddress; //признаки корректности адреса, устанавливается в "false" в случае отстутствия названия улицы или номера дома
+    //    public string index;
+    //    public string region;
+    //    public string area;
+    //    public string city;
+    //    public string street;
+    //    public string house;
+    //    public string flat;
+    //}
+
 }
