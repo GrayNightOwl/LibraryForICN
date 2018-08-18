37220395c5e4a47d17a1459cca3394cebeb37279
Сергей <serega-k97@yandex.ru>
2018-08-19 01:17:51 +0500

    Программа работает с библиотекой, код которой улавливает исключения

--- WCFServiceForAdress/Adress.svc.cs
+++ WCFServiceForAdress/Adress.svc.cs
@@ -13,7 +13,7 @@ namespace WCFServiceForAdress
     public class Service1 : IntAddress
     {
         AdressFromLIB addr = new AdressFromLIB();
-
+        
         public string CompileAddressFromSet(string index, string region, string area, string city, string street, string house, string flat)
         {   //создаёт объект типа Адрес, передаёт в функцию сборки из объекта 
 
@@ -33,69 +33,14 @@ namespace WCFServiceForAdress
 
         public string[] ParseAddress(string s)
         {
-
-            string[] mas = new string[7];
-            AddressStructure result = new AddressStructure();
-            result.CorrectAddress = true; //изначально считаем адрес корректным
-            s = "," + s + ",";           //"окаймим" строку запятыми для отделения участков по запятым с двух сторон
-            result.index = Index(s);     //может вернуть индекс по умолчанию
-            result.region = Region(s);   //может вернуть регион по умолчанию
-            result.area = Area(s);       //не вернёт район по умолчанию, иначе все адреса будут сельскими
-            result.city = City(s);       //может вернуть город по умолчанию
-            result.street = Street(s);   //если не указано - вернуть ошибку
-            result.house = House(s);     //если не указано - вернуть ошибку
-            if ((result.street == "Не удалось распознать улицу") || (result.house == "Не удалось распознать дом"))
-            {
-                result.CorrectAddress = false; //в случае отсутствия улицы/дома считаем адрес некорректным
-                                               //return CompileAddress(result);
-            }
-            else
-
-            {
-                result.flat = Flat(s);   //не вернёт значение по умолчанию, но адрес может существовать и без квартиры
-                mas[6] = result.flat;
-            }
-            //return CompileAddress(result);
-            mas[0] = result.index;
-            mas[1] = result.region;
-            mas[2] = result.area;
-            mas[3] = result.city;
-            mas[4] = result.street;
-            mas[5] = result.house;
-            return mas;
+            return addr.ParseAddress(s);
         } //парсер адреса из строки, получает объект, преобразует в строку с помощью CompileAdress
-
 
         private string CompileAddress(AddressStructure adress)
         {
-            string result = "";
-
-            if (adress.index != "Не удалось распознать индекс")
-            {
-                result = result + adress.index + ", ";  //индекс не назначается по умолчанию, опустим его
-            }
-
-            result = result + adress.region + ", "; //регион назначается по умолчанию, можно прибавлять и ставить запятую
-
-            if (adress.area != "") //данная проверка необходима, чтобы добавлять запятую только в необходимых случаях
-            {                       //значение по умолчанию для района не ставится, нужно иметь городские и сельские адреса
-                result = result + adress.area + ", ";
-            }
-            result = result + "г. " + adress.city + ", "; //город назначается по умолчанию, добавляем без раздумий
-            if (adress.CorrectAddress == true) //если адрес корректный - то улица и дом распознаны, прибавим их
-            {
-                result = result + "ул. " + adress.street + ", ";
-                result = result + "д. " + adress.house;
-                if ((adress.flat != "Не удалось распознать квартиру") && (adress.flat != ""))
-                {
-                    result = result + ", кв. " + adress.flat;
-                }
-            }
-            else { result = "Некорректный адрес"; }; //иначе вернём ошибку распознавания
-
-            return result;
+            return addr.CompileAddress(adress);
         }
-        
+
         private string Index(string s)
         {
             return addr.Index(s);
