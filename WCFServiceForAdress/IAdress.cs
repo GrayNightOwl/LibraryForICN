3d236c84d337ca21d023faaaed2e40bc71b7ff48
Сергей <serega-k97@yandex.ru>
2018-08-19 00:29:02 +0500

    Лишний код удалён, программа работает с функциями из библиотеки

--- WCFServiceForAdress/IAdress.cs
+++ WCFServiceForAdress/IAdress.cs
@@ -19,18 +19,4 @@ namespace WCFServiceForAdress
         string[] ParseAddress(string s);
 
     }
-
-    //[DataContract]
-    //public class AddressStructure
-    //{
-    //    public bool CorrectAddress; //признаки корректности адреса, устанавливается в "false" в случае отстутствия названия улицы или номера дома
-    //    public string index;
-    //    public string region;
-    //    public string area;
-    //    public string city;
-    //    public string street;
-    //    public string house;
-    //    public string flat;
-    //}
-
 }
