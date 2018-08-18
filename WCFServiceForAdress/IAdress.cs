007721ff17638ba50de5c3be2d585626433f65ef
Сергей <serega-k97@yandex.ru>
2018-08-18 18:12:46 +0500

    Попытаемся запустить с возвратом объекта

--- WCFServiceForAdress/IAdress.cs
+++ WCFServiceForAdress/IAdress.cs
@@ -18,33 +18,6 @@ namespace WCFServiceForAdress
         AdressStructure ParseAdress(string s);
     }
 
-    //#region Adress
-
-    /////<compile>
-    /////собрать
-    /////</compile>>
-    /////<param name="Adress"> Адрес </param>
-    /////<returns> </returns>
-
-
-
-    //#endregion
-
-    //// ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени интерфейса "IService1" в коде и файле конфигурации.
-    //[ServiceContract]
-    //public interface IAdress
-    //{
-
-    //    [OperationContract]
-    //    string GetData(int value);
-
-    //    [OperationContract]
-    //    CompositeType GetDataUsingDataContract(CompositeType composite);
-
-    //    // TODO: Добавьте здесь операции служб
-    //}
-
-
     // Используйте контракт данных, как показано в примере ниже, чтобы добавить составные типы к операциям служб.
     [DataContract]
     public class AdressStructure
@@ -57,20 +30,6 @@ namespace WCFServiceForAdress
         public string street;
         public string house;
         public string flat;
-
-
-        //[DataMember]
-        //public bool BoolValue
-        //{
-        //    get { return boolValue; }
-        //    set { boolValue = value; }
-        //}
-
-        //[DataMember]
-        //public string StringValue
-        //{
-        //    get { return stringValue; }
-        //    set { stringValue = value; }
-        //}
     }
+
 }
