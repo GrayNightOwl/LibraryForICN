e0bf1c8b7cb335496fd62d9b7ca1e6c353726f0d
Сергей <serega-k97@yandex.ru>
2018-08-18 17:47:42 +0500

    Создал интерфейс и реализацию работы с адресами. Переместил содержимое класса работы с адресом в реализацию интерфейса WCF сервиса. Пока не знаю, как обратиться из него в другой файл, чтобы подтянуть реализацию функций оттуда.

--- WCFServiceForAdress/IAdress.cs
+++ WCFServiceForAdress/IAdress.cs
@@ -5,18 +5,72 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
+using LibraryForICN;
 
 namespace WCFServiceForAdress
 {
-    [ServiceContract(Namespace = "AddressNS")]
-    public interface IntAddress
+    [ServiceContract(Namespace = "AdressNamespace")]
+    public interface IntAdress
     {
-        
         [OperationContract]
-        string CompileAddressFromSet(string index, string region, string area, string city, string street, string house, string flat);
-
+        string CompileAdress(AdressStructure adress);
         [OperationContract]
-        string[] ParseAddress(string s);
+        AdressStructure ParseAdress(string s);
+    }
 
+    //#region Adress
+
+    /////<compile>
+    /////собрать
+    /////</compile>>
+    /////<param name="Adress"> Адрес </param>
+    /////<returns> </returns>
+
+
+
+    //#endregion
+
+    //// ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени интерфейса "IService1" в коде и файле конфигурации.
+    //[ServiceContract]
+    //public interface IAdress
+    //{
+
+    //    [OperationContract]
+    //    string GetData(int value);
+
+    //    [OperationContract]
+    //    CompositeType GetDataUsingDataContract(CompositeType composite);
+
+    //    // TODO: Добавьте здесь операции служб
+    //}
+
+
+    // Используйте контракт данных, как показано в примере ниже, чтобы добавить составные типы к операциям служб.
+    [DataContract]
+    public class AdressStructure
+    {
+        public bool CorrectAdress; //признаки корректности адреса, устанавливается в "false" в случае отстутствия названия улицы или номера дома
+        public string index;
+        public string region;
+        public string area;
+        public string city;
+        public string street;
+        public string house;
+        public string flat;
+
+
+        //[DataMember]
+        //public bool BoolValue
+        //{
+        //    get { return boolValue; }
+        //    set { boolValue = value; }
+        //}
+
+        //[DataMember]
+        //public string StringValue
+        //{
+        //    get { return stringValue; }
+        //    set { stringValue = value; }
+        //}
     }
 }
