7c7516d7b8a73b6f48a0a70a72e3c0ed1e1fb13a
Сергей <serega-k97@yandex.ru>
2018-08-25 23:59:52 +0500

    Дописаны комментарии в реализации сервиса

--- WCFServiceForAdress/Adress.svc.cs
+++ WCFServiceForAdress/Adress.svc.cs
@@ -12,13 +12,26 @@ namespace WCFServiceForAdress
 {
     public class Service1 : IntAddress
     {
-
+        /// <summary>
+        /// Метод, использующийся для сборки адреса из отдельных частей
+        /// Использует метод преобразования к библиотечному классу, затем использует библиотечный метод.
+        /// </summary>
+        /// <param name="address">Адресс, принятый с помощью WCF, объект класса AddressTransfer</param>
+        /// <returns>Строка с собранным адресом</returns>
         public string CompileAddress(AddressTransfer address)
         {
             AddressStructure temp = address.ConvertToAddressStructure();
             return temp.CompileAddress();
         }
 
+        /// <summary>
+        /// Метод, использующийся для разборки адреса
+        /// Создаётся экземпляр библиотечного класса, с помощью конструктора разбирается адрес
+        /// Создаётся экземпляр класса AddressTranfser, пригодного для передачи с помощью WCF
+        /// Если адрес корректный- распознанные значения полей библиотечного класса присваиваются полям класса для передачи
+        /// </summary>
+        /// <param name="s">Строка с нечётким адресом</param>
+        /// <returns>Объект, пригодный для передачи с помощью WCF с распознанным адресом или флагом и сообщением об ошибки</returns>
         public AddressTransfer ParseAddress(string s)
         {
             AddressStructure addr = new AddressStructure(s);
@@ -32,10 +45,13 @@ namespace WCFServiceForAdress
                 ATresult.street = addr.Street;
                 ATresult.house = addr.House;
                 ATresult.flat = addr.Flat;
-                ATresult.error = addr.Error;
+                ATresult.error = addr.Error; //на случай, если флаг корректности установлен, но есть ошибка
             }
             else
+            {
                 ATresult.CorrectAddress = false; //как это передаётся пользователю?
+                ATresult.error = addr.Error;
+            }
             return ATresult;
         }
         
