9d1946e562cbf9f891b47b69962636e22300cc88
GrayNightOwl <42420228+GrayNightOwl@users.noreply.github.com>
2018-08-26 01:04:25 +0500

    Add files via upload

--- WCFServiceForAdress/IAdress.cs
+++ WCFServiceForAdress/IAdress.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
+using LibraryForICN;
 using System.ServiceModel.Web;
+using System.Text.RegularExpressions;
 using System.Text;
 
 namespace WCFServiceForAdress
@@ -11,12 +13,90 @@ namespace WCFServiceForAdress
     [ServiceContract(Namespace = "AddressNS")]
     public interface IntAddress
     {
-        
+        /// <summary>
+        /// Описание метода, используемого для сборки адреса из объекта, полученного с помощью WCF
+        /// </summary>
+        /// <param name="address">Объект типа AddressTransfer, полученный с помощью WCF</param>
+        /// <returns>Собранная строка</returns>
         [OperationContract]
-        string CompileAddressFromSet(string index, string region, string area, string city, string street, string house, string flat);
+        string CompileAddress(AddressTransfer address);
 
+        /// <summary>
+        /// Описание метода, используемого для разборки нечётко заданного адреса
+        /// </summary>
+        /// <param name="s">Строка с нечётко заданным адресом</param>
+        /// <returns>Объект типа AddressTransfer, пригодный для передачи с помощью WCF</returns>
         [OperationContract]
-        string[] ParseAddress(string s);
+        AddressTransfer ParseAddress(string s);
+
+
+    }
+
+
+    /// <summary>
+    /// Соглашение о классе AddressTranfser
+    /// </summary>
+    [DataContract]
+    public class AddressTransfer
+    {
+        /// <summary>
+        /// Метод, преобразующий класс AddressTransfer (передача WCF) в класс AdressStructure, работа с которым идёт в библиотеке
+        /// Содержит проверку на пустоту квартиры и улицы, так как именно этот набор является минимально допустимым
+        /// </summary>
+        /// <returns>объект типа AddressStructure, работа с которым потом пойдёт методами из библиотеки</returns>
+        public AddressStructure ConvertToAddressStructure()
+        {
+            AddressStructure temp = new AddressStructure();
+            if ((street == "") || (house == ""))
+            {
+                temp.CorrectAddress = false;
+            }
+            else
+            {
+                temp.CorrectAddress = true; //искуственно выставляем флаг корректности, так как необходимо 
+                temp.Index = index;
+                temp.Region = region;
+                temp.Area = area;
+                temp.City = city;
+                temp.Street = street;
+                temp.House = house;
+                temp.Flat = flat;
+            }
+            return temp;
+        }
+
+
+        /// <summary>
+        /// Далее следуют соглашения о полях, передаваемых с помощью WCF
+        /// </summary>
+        [DataMember]
+        public string index;
+
+        [DataMember]
+        public string region;
+
+        [DataMember]
+        public string area;
+
+        [DataMember]
+        public string city;
+
+        [DataMember]
+        public string street;
+
+        [DataMember]
+        public string house;
+
+        [DataMember]
+        public string flat;
+
+        [DataMember]
+        public string error;
+
+        /// <summary>
+        /// Метод, который не передаётся при взаимодействии с помощью WCF
+        /// </summary>
+        public bool CorrectAddress;
 
     }
 }
